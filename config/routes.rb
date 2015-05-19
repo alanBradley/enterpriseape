2653dc6f858104bd999d1dd97e24d41537a4820e
Bradley <alanbradley@live.ie>
2015-05-19 09:48:25 +0000

    purchase changes but getting routing error which cannot fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
-Rails.application.routes.draw do
-  
+Enterpriseape::Application.routes.draw do
+
+
+  resources :items
+
   resources :employees
 
-  resources :companies do
-    collection { post :import}
+  resources :companies do 
+    collection { post :import }
   end
-  
 
   devise_for :users
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
-  get 'welcome/faq'
-  get 'welcome/pricing'
-  get 'welcome/features'
-
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/contact"
+  get "welcome/faq"
+  get "welcome/pricing"
+  get "welcome/features"
+  
   resources :invoices do
-    # blocked the method call index as will never list out purchases without connecting it to the invoice
     resources :purchases, except: [:index], controller: 'invoices/purchases'
   end
-  
-  
+
   root to: 'welcome#index'
   
 end
